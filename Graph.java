767351a641df9fe5ae6f259d0957f2a611069e15
CodeProdigy88 <cameronmckay88@gmail.com>
2026-06-22 19:20:54 -0600

    Add files via upload

---
+++ Graph.java
@@ -0,0 +1,22 @@
+package assign07;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Graph<Type> {
+
+	public boolean depthFirstSearch(Type source, Type destination) {
+		return false;
+	}
+
+	public List<Type> breadthFirstSearch(Type source, Type destination) {
+		List<Type> returnList = new LinkedList<Type>();
+		return returnList;
+	}
+
+	public List<Type> topoSort() {
+		List<Type> returnList = new LinkedList<Type>();
+		return returnList;
+	}
+
+}
---
+++ GraphUtility.java
@@ -0,0 +1,23 @@
+package assign07;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class GraphUtility<Type> {
+
+	public static <Type> boolean areConnected(List<Type> sources, List<Type> destinations, Type srcData, Type dstData) {
+		return false;
+	}
+
+	public static <Type> List<Type> shortestPath(List<Type> sources, List<Type> destinations, Type srcData,
+			Type dstData) {
+		List<Type> returnList = new LinkedList<Type>();
+		return returnList;
+	}
+
+	public static <Type> List<Type> sort(List<Type> sources, List<Type> destinations) {
+		List<Type> returnList = new LinkedList<Type>();
+		return returnList;
+	}
+
+}
